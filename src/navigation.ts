fd7173ceb1431600e5ca25423e834f5c1b0f7bec
mike-klein1 <mike.klein.88@gmail.com>
2026-01-19 14:08:24 +0100

    navigation fix

--- src/navigation.ts
+++ src/navigation.ts
@@ -42,3 +42,36 @@ export const headerData = {
     },
   ],
 };
+export const footerData = {
+  links: [
+    {
+      title: 'Leistungen',
+      links: [
+        { text: 'Planung', href: '/planung' },
+        { text: 'Integration', href: '/integration' },
+        { text: 'Wartung', href: '/wartung' },
+      ],
+    },
+    {
+      title: 'Unternehmen',
+      links: [
+        { text: 'Über mich', href: '/ueber-mich' },
+        { text: 'Blog', href: '/blog' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Impressum', href: '/impressum' },
+    { text: 'Datenschutz', href: '/datenschutz' },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: '/rss.xml' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/dein-nutzername' },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="/"> HomeIQ</a> · All rights reserved.
+  `,
+};
